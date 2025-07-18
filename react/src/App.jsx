26511bef7f6838fe462fce0a261c1373aa11a35a
Luke Sontrop <lukerobertsontrop@gmail.com>
2025-07-18 12:27:32 -0700

    Updates react home page. Adds some assets.

--- react/src/App.jsx
+++ react/src/App.jsx
@@ -1,6 +1,5 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import bluesaltlabsLogo from './assets/bluesaltlabs-logo.svg'
 import './App.css'
 
 function App() {
@@ -9,14 +8,12 @@ function App() {
   return (
     <>
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
+        <a href="https://github.com/bluesaltlabs" target="_blank">
+          <img src={bluesaltlabsLogo} className="logo BlueSaltLabs" alt="BlueSaltLabs logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
+      <h1>BlueSalt Labs</h1>
+      <h2>( + Vite + React )</h2>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
@@ -26,7 +23,7 @@ function App() {
         </p>
       </div>
       <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
+        Clicking the logo will lead to more information.
       </p>
     </>
   )
